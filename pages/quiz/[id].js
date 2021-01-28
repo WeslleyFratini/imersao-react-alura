3c9275622d37167a8c4197b083b8e079acc098c3
Weslley Fratini <weeslleey354@gmail.com>
2021-01-28 19:52:00 -0300

    alterações no app

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,10 +1,5 @@
-  
-import React from 'react';
+import React from "react";
 
 export default function QuizDaGaleraPage() {
-  return (
-    <div>
-      Desafio da próxima aula junto com as animações
-    </div>
-  );
+  return <div>Desafio da próxima aula junto com as animações</div>;
 }
